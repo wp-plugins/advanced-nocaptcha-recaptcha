b139b42ea494e6434ace8e61cdaabfa284fc339f
shamim51 <shamim51@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-27 07:03:04 +0000

    First commit
    git-svn-id: https://plugins.svn.wordpress.org/advanced-nocaptcha-recaptcha/tags/1.1@1146767 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ anr-captcha-class.php
@@ -0,0 +1,188 @@
+<?php
+
+if (!class_exists('anr_captcha_class'))
+{
+  class anr_captcha_class
+  {
+ 	private static $instance;
+	
+	public static function init()
+        {
+            if(!self::$instance instanceof self) {
+                self::$instance = new self;
+            }
+            return self::$instance;
+        }
+		
+    function actions_filters()
+    	{
+			if ( '1' == anr_get_option( 'fep_contact_form' )) {
+					add_action ('fepcf_message_form_after_content', array(&$this, 'form_field'), 99);
+					add_action ('fepcf_action_message_before_send', array(&$this, 'fepcf_verify'));
+				}
+			
+			if ( '1' == anr_get_option( 'login' )) {
+					add_action ('login_form', array(&$this, 'form_field'), 99);
+					add_filter ('wp_authenticate_user', array(&$this, 'login_verify'), 10, 2 );
+				}
+			
+			if ( '1' == anr_get_option( 'registration' )) {
+					add_action ('register_form', array(&$this, 'form_field'), 99);
+					add_filter ('registration_errors', array(&$this, 'registration_verify'), 10, 3 );
+				}
+			
+			if ( '1' == anr_get_option( 'lost_password' )) {
+					add_action ('lostpassword_form', array(&$this, 'form_field'), 99);
+					add_action ('allow_password_reset', array(&$this, 'lostpassword_verify'), 10, 2); //lostpassword_post does not return wp_error
+				}
+				
+			if ( '1' == anr_get_option( 'reset_password' )) {
+					add_action ('resetpass_form', array(&$this, 'form_field'), 99);
+					add_filter ('validate_password_reset', array(&$this, 'reset_password_verify'), 10, 2 );
+				}
+					
+			if ( '1' == anr_get_option( 'comment' )) {
+					add_filter ('comment_form_field_comment', array(&$this, 'comment_form_field') );
+					add_filter ('pre_comment_on_post', array(&$this, 'comment_verify') );
+				}
+			
+			if ( function_exists( 'wpcf7_add_shortcode' )) {
+					wpcf7_add_shortcode('anr_nocaptcha', array(&$this, 'wpcf7_form_field'), true);
+					add_filter('wpcf7_validate_anr_nocaptcha', array(&$this, 'wpcf7_verify'), 10, 2);
+				}
+    	}
+		
+	
+	function form_field()
+		{
+			$loggedin_hide 	= anr_get_option( 'loggedin_hide' ); 
+			
+			if ( is_user_logged_in() && $loggedin_hide )
+				return;
+				
+			echo anr_captcha_form_field( false );
+			
+		}
+		
+	function comment_form_field( $defaults )
+		{
+			$loggedin_hide 	= anr_get_option( 'loggedin_hide' ); 
+			
+			if ( is_user_logged_in() && $loggedin_hide )
+				return $defaults;
+				
+				$defaults = $defaults. '<br />' .anr_captcha_form_field( false );
+				return $defaults;
+			
+			
+		}
+		
+	function verify()
+		{
+			
+			$loggedin_hide 	= anr_get_option( 'loggedin_hide' ); 
+			
+			if ( is_user_logged_in() && $loggedin_hide )
+				return true;
+				
+			return anr_verify_captcha();
+			
+		}
+		
+	function fepcf_verify ( $errors )
+		{
+			$error_message = str_replace(__('<strong>ERROR</strong>: ', 'anr'), '', anr_get_option( 'error_message' ));
+			
+			if ( ! $this->verify() )
+			
+			$errors->add('anr_error', $error_message);
+		}
+		
+	function login_verify (  $user, $password )
+		{
+			if ( ! $this->verify() ) {
+			$error_message = anr_get_option( 'error_message' );
+			return new WP_Error( 'anr_error', $error_message );
+			}
+			
+			return $user;
+		}
+		
+	function registration_verify (  $errors, $sanitized_user_login, $user_email )
+		{
+			if ( ! $this->verify() ) {
+			$error_message = anr_get_option( 'error_message' );
+			$errors->add( 'anr_error', $error_message );
+			}
+			
+			return $errors;
+		}
+		
+	function lostpassword_verify( $result, $user_id )
+		{
+			if ( ! $this->verify() ) {
+			$error_message = anr_get_option( 'error_message' );
+			return new WP_Error( 'anr_error', $error_message );
+			}
+			
+			return $result;
+		}
+		
+		
+	function reset_password_verify( $errors, $user )
+		{
+			
+			if ( ! $this->verify() ) {
+			$error_message = anr_get_option( 'error_message' );
+			$errors->add('anr_error', $error_message);
+				}
+		}
+		
+	function comment_verify( $errors, $user )
+		{
+			
+			if ( ! $this->verify() ) {
+			$error_message = anr_get_option( 'error_message' );
+			wp_die( $error_message, 200 );
+				}
+		}
+		
+	function wpcf7_form_field( $tags )
+		{
+			$loggedin_hide 	= anr_get_option( 'loggedin_hide' ); 
+			
+			if ( is_user_logged_in() && $loggedin_hide )
+				return;
+				
+				return anr_captcha_form_field( false )."<span class='wpcf7-form-control-wrap g-recaptcha-response'></span>";
+			
+			
+		}
+		
+	function wpcf7_verify( $result, $tag  )
+		{
+			$tag = new WPCF7_Shortcode( $tag );
+			$name = $tag->name;
+			
+			if ( ! $this->verify() ) {
+			
+				$error_message = anr_get_option( 'error_message' ).'<button onclick="javascript:location.reload();">Reload Captcha</button>';
+				
+				if ( method_exists($result, 'invalidate' ) ) { // wpcf7 4.1
+				$result->invalidate( $tag, $error_message );
+			} else {
+				$result['valid'] = false;
+				$result['reason'][$name] = $error_message;
+			}
+				}
+
+		return $result;
+		}
+
+
+	
+  } //END CLASS
+} //ENDIF
+
+add_action('init', array(anr_captcha_class::init(), 'actions_filters'));
+?>
